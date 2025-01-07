1eeb0cf4fd983dc6a89c48116eab70514d4fc533
Silas Schnurr <silas.schnurr@peakavenue.com>
2025-01-07 18:57:00 +0100

    Init

---
+++ Frontend/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+	Visit <a href="https://svelte.dev/tutorial/svelte/">svelte.dev/tutorial/svelte</a> to open the tutorial
+</p>
